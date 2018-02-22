73c06337fb9a7c2de2a1aacaef3d3e140603f7c8
sergey.cherkesov <sergey.cherkasov@devim.team>
2018-02-22 17:26:45 +0300

    Add simple server

--- geometry_util.py
+++ geometry_util.py
@@ -5,6 +5,7 @@ DIRECTION_LEFT = "left"
 DIRECTION_RIGHT = "right"
 DIRECTION_NONE = "none"
 
+
 def contour_center(c):
     m = cv2.moments(c)
     x = int((m["m10"] / m["m00"]))
@@ -23,19 +24,20 @@ def vectors_angle(x1, y1, x2, y2):
     return res
 
 
-def direction_change(x1, y1, x2, y2, x3, y3):
-    a1 = vectors_angle(x1, y1, x2, y2)
-    a2 = vectors_angle(x2, y2, x3, y3)
+# def direction_change(x1, y1, x2, y2, x3, y3):
+#     a1 = vectors_angle(x1, y1, x2, y2)
+#     a2 = vectors_angle(x2, y2, x3, y3)
+#
+#     a1a2 = a2 - a1
+#     # a2a1 = a1 - a2
+#     a2a1 = (-1 * a1 + 360) - (-1 * a2 + 360)
+#
+#     d = a1a2 if abs(a1a2) < abs(a2a1) else a2a1
+#     return DIRECTION_RIGHT if d > 0 else DIRECTION_LEFT
 
-    a1a2 = a2 - a1
-    # a2a1 = a1 - a2
-    a2a1 = (-1 * a1 + 360) - (-1 * a2 + 360)
 
-    d = a1a2 if abs(a1a2) < abs(a2a1) else a2a1
-    return DIRECTION_RIGHT if d > 0 else DIRECTION_LEFT
-
-def direction_change2(ax, ay, bx, by, px, py):
-    s = (bx-ax)*(py-ay)-(by-ay)*(px-ax);
-    if s > 0 return DIRECTION_LEFT
-    if s < 0 return DIRECTION_RIGHT
+def direction_change(ax, ay, bx, by, px, py):
+    s = (bx - ax) * (py - ay) - (by - ay) * (px - ax)
+    if s > 0: return DIRECTION_LEFT
+    if s < 0: return DIRECTION_RIGHT
     return DIRECTION_NONE
